cf3137881984b37d793adf88fdb0c211c4dacf8f
Kvalyr <kvalyr@valyr.net>
2023-09-15 16:53:07 +0200

    0.4.5 - Added workaround for temp followers by Elys@NexusMods.

--- KvCampEvents/Mods/KvCampEvents/ScriptExtender/Lua/BootstrapServer.lua
+++ KvCampEvents/Mods/KvCampEvents/ScriptExtender/Lua/BootstrapServer.lua
@@ -7,7 +7,7 @@
 KVS = {}
 KVS.modTableKey = "KvCampEvents"
 KVS.modPrefix = "KvCE"
-KVS.modVersion = {major="0.4", minor="4"}
+KVS.modVersion = {major="0", minor="4", revision="5", build="0"}
 
 -- KvShared
 Ext.Require("KvShared/_Main.lua")
